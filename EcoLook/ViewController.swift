68a356512eb616f833058596174c800afd561ac7
Brian Sanchez <bsanchso@LIM-FVFHL3GUQ05N.local>
2022-06-16 17:50:51 -0500

    Mi primer commit

---
+++ EcoLook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EcoLook
+//
+//  Created by Brian Antonio Sanchez Solorsano on 16/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
