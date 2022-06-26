2daafb508d6380400a8463ab022b08328e5500cc
Brian Sanchez <bsanchso@emeal.nttdata.com>
2022-06-26 09:39:40 -0500

    CollectionViewCell para tags creado

---
+++ EcoLook/CollectionViewCell/TagsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TagsCollectionViewCell.swift
+//  EcoLook
+//
+//  Created by Brian Antonio Sanchez Solorsano on 26/06/22.
+//
+
+import UIKit
+
+class TagsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
