608b10fc88989ddfc2da81a7ad00949f68ff2330
Brian Sanchez <bsanchso@emeal.nttdata.com>
2022-06-26 22:48:29 -0500

    PostTableViewCell agregado y constraints aplicados

---
+++ EcoLook/TableViewCell/PostsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostsTableViewCell.swift
+//  EcoLook
+//
+//  Created by Brian Antonio Sanchez Solorsano on 26/06/22.
+//
+
+import UIKit
+
+class PostsTableViewCell: UITableViewCell {
+    @IBOutlet weak var viewCard: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
