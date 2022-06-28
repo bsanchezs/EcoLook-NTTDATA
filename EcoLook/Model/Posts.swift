3f1077669edfb471f0eb55390e9786633bbdb04d
Brian Sanchez <bsanchso@emeal.nttdata.com>
2022-06-28 17:19:30 -0500

    Propiedad direccion agregada al struct Post y al mock array

--- EcoLook/Model/Posts.swift
+++ EcoLook/Model/Posts.swift
@@ -13,6 +13,7 @@ struct Post {
     let titulo: String
     let descripcion: String
     let nivelRiesgo: Int
+    let direccion: String
     let latitud: Double?
     let longitud: Double?
     let imagen: String?
@@ -27,6 +28,7 @@ struct Post {
         self.titulo = "Sin Titulo"
         self.descripcion = "Sin Desc"
         self.nivelRiesgo = 0
+        self.direccion = ""
         self.latitud = nil
         self.longitud = nil
         self.imagen = nil
@@ -37,12 +39,13 @@ struct Post {
         
     }
     
-    init (idPublicacion: Int, titulo: String, descripcion: String, nivelRiesgo: Int, fecha: String, arrayEtiquetas: [Tag], idUsuario: Int){
+    init (idPublicacion: Int, titulo: String, descripcion: String, nivelRiesgo: Int, direccion: String, fecha: String, arrayEtiquetas: [Tag], idUsuario: Int){
         
         self.idPublicacion = idPublicacion
         self.titulo = titulo
         self.descripcion = descripcion
         self.nivelRiesgo = nivelRiesgo
+        self.direccion = direccion
         self.latitud = nil
         self.longitud = nil
         self.imagen = nil
@@ -67,10 +70,10 @@ struct Post {
 
 let arrMockPosts = [
 
-    Post(idPublicacion: 1, titulo: "Publicacion 1", descripcion: "Primera descripcion", nivelRiesgo: 3, fecha: "10/02/2021", arrayEtiquetas: [Tag(id: 1, name: "#basura", status: 1), Tag(id: 2, name: "#plastico", status: 1)], idUsuario: 1),
-    Post(idPublicacion: 2, titulo: "Publicacion 2", descripcion: "Segunda descripcion", nivelRiesgo: 1, fecha: "11/02/2021", arrayEtiquetas: [Tag(id: 3, name: "#playa", status: 1)], idUsuario: 1),
-    Post(idPublicacion: 3, titulo: "Publicacion 3", descripcion: "Tercera descripcion", nivelRiesgo: 4, fecha: "11/02/2021", arrayEtiquetas: [Tag(id: 5, name: "#desechos_toxicos", status: 1), Tag(id: 1, name: "#basura", status: 1), Tag(id: 6, name: "#urgente", status: 1)], idUsuario: 1),
-    Post(idPublicacion: 4, titulo: "Publicacion 4", descripcion: "Cuarta descripcion", nivelRiesgo: 2, fecha: "12/02/2021", arrayEtiquetas: [], idUsuario: 1),
+    Post(idPublicacion: 1, titulo: "Publicacion 1", descripcion: "Primera descripcion", nivelRiesgo: 3, direccion: "direccion 1", fecha: "10/02/2021", arrayEtiquetas: [Tag(id: 1, name: "#basura", status: 1), Tag(id: 2, name: "#plastico", status: 1)], idUsuario: 1),
+    Post(idPublicacion: 2, titulo: "Publicacion 2", descripcion: "Segunda descripcion", nivelRiesgo: 1, direccion: "direccion 2", fecha: "11/02/2021", arrayEtiquetas: [Tag(id: 3, name: "#playa", status: 1)], idUsuario: 1),
+    Post(idPublicacion: 3, titulo: "Publicacion 3", descripcion: "Tercera descripcion", nivelRiesgo: 4, direccion: "direccion 3", fecha: "11/02/2021", arrayEtiquetas: [Tag(id: 5, name: "#desechos_toxicos", status: 1), Tag(id: 1, name: "#basura", status: 1), Tag(id: 6, name: "#urgente", status: 1)], idUsuario: 1),
+    Post(idPublicacion: 4, titulo: "Publicacion 4", descripcion: "Cuarta descripcion", nivelRiesgo: 2, direccion: "direccion 4", fecha: "12/02/2021", arrayEtiquetas: [], idUsuario: 1),
 
 ]
 
