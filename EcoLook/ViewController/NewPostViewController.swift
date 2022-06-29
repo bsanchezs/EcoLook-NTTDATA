b616e4fcf85a2063d7afbda238b161f2c7351e26
Brian Sanchez <bsanchso@emeal.nttdata.com>
2022-06-28 23:18:32 -0500

    NewPostViewController agregado - label y btn del header colocados y enlazados

---
+++ EcoLook/ViewController/NewPostViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NewPostViewController.swift
+//  EcoLook
+//
+//  Created by Brian Antonio Sanchez Solorsano on 28/06/22.
+//
+
+import UIKit
+
+class NewPostViewController: UIViewController {
+
+    @IBOutlet weak var labelTitleHeader: UILabel!
+    @IBOutlet weak var buttonGoBackHeader: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
