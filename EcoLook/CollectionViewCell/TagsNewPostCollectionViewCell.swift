52477965dd3d8c06f2386ac6fa2d2e90123f84d5
Brian Sanchez <bsanchso@emeal.nttdata.com>
2022-06-30 10:45:07 -0500

    CollectionView para las etiquetas colocado y enlazado. TagsNewPostCollectionViewCell agregado y modificado para la construccion de las celdas asi como tambien implementado su comportamiento con los botones hacia la vista

---
+++ EcoLook/CollectionViewCell/TagsNewPostCollectionViewCell.swift
@@ -0,0 +1,62 @@
+//
+//  TagsNewPostCollectionViewCell.swift
+//  EcoLook
+//
+//  Created by Brian Antonio Sanchez Solorsano on 29/06/22.
+//
+
+import UIKit
+
+protocol ButtonNewPostViewCollectionCellDelegate: AnyObject{
+    
+    func btnAddDeleteTagNewPost(btnTagName: String?)
+    
+}
+
+class TagsNewPostCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var buttonTagNewPost: UIButton!
+    
+    var tagNewPost: [Tag]?
+    var delegate: ButtonNewPostViewCollectionCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.backgroundColor = ThemeColors.verdeBackground
+        
+        
+        buttonTagNewPost.tintColor = ThemeColors.verdeLabel
+        buttonTagNewPost.backgroundColor = ThemeColors.verdeBackground
+        buttonTagNewPost.layer.cornerRadius = 15
+        
+        
+        tagNewPost = arrEtiquetasTotales.filter( {$0.id == buttonTagNewPost.tag} )
+//        buttonTagNewPost.setTitle(tagNewPost?.name, for: .normal)
+        
+//        guard let id = tagNewPost?.id else{
+//            return
+//        }
+//        buttonTagNewPost.tag = id
+        
+    }
+    
+    
+    @IBAction func btnTagAction(_ sender: UIButton) {
+        
+        if sender.backgroundColor == ThemeColors.verdeBackground {
+            
+            sender.backgroundColor = ThemeColors.verdePrimary
+            sender.tintColor = .white
+            
+        }else{
+            
+            sender.backgroundColor = ThemeColors.verdeBackground
+            sender.tintColor = ThemeColors.verdeLabel
+            
+        }
+        
+        delegate?.btnAddDeleteTagNewPost(btnTagName: sender.currentTitle)
+    }
+    
+}
--- EcoLook/ViewController/NewPostViewController.swift
+++ EcoLook/ViewController/NewPostViewController.swift
@@ -18,6 +18,10 @@ class NewPostViewController: UIViewController {
     @IBOutlet weak var buttonDirectionNewPost: UIButton!
     @IBOutlet weak var textViewDescriptionNewPost: UITextView!
     @IBOutlet weak var sliderRiskLevelNewPost: UISlider!
+    @IBOutlet weak var collectionViewTagsNewPost: UICollectionView!
+    
+    var newPost = Post()
+    var setTagsSelectedNewPost = Set<String>()
     
     
     override func viewDidLoad() {
@@ -31,12 +35,18 @@ class NewPostViewController: UIViewController {
         imageViewPhotoNewPost.layer.cornerRadius = 15
         
         buttonDirectionNewPost.tintColor = ThemeColors.verdePrimary
+        
         textViewDescriptionNewPost.backgroundColor = .white
         textViewDescriptionNewPost.layer.borderWidth = 1
         textViewDescriptionNewPost.layer.borderColor = .init(red: 0, green: 0, blue: 0, alpha: 0.08)
         textViewDescriptionNewPost.layer.cornerRadius = 15
+        
         sliderRiskLevelNewPost.thumbTintColor = ThemeColors.verdePrimary
         sliderRiskLevelNewPost.tintColor = ThemeColors.verdePrimary
+        
+        collectionViewTagsNewPost.showsVerticalScrollIndicator = false
+        collectionViewTagsNewPost.backgroundColor = ThemeColors.verdeBackground
+        collectionViewTagsNewPost.dataSource = self
         
 
         
@@ -47,4 +57,62 @@ class NewPostViewController: UIViewController {
     }
     
 
+}
+
+extension NewPostViewController: UICollectionViewDataSource{
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return arrEtiquetasTotales.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customCellTagNewPost", for: indexPath) as? TagsNewPostCollectionViewCell
+        
+        cell?.buttonTagNewPost.setTitle(arrEtiquetasTotales[indexPath.row].name, for: .normal)
+        
+        cell?.buttonTagNewPost.tag = arrEtiquetasTotales[indexPath.row].id
+        
+        cell?.delegate = self
+        
+        return cell!
+        
+        
+    }
+    
+    
+    
+}
+
+extension NewPostViewController: ButtonNewPostViewCollectionCellDelegate{
+    
+    func btnAddDeleteTagNewPost(btnTagName: String?) {
+        
+        guard let tagName = btnTagName else{
+            return
+        }
+        
+        if setTagsSelectedNewPost.contains(tagName) {
+            
+            setTagsSelectedNewPost.remove(tagName)
+            
+        }else{
+        
+            setTagsSelectedNewPost.insert(tagName)
+            
+        }
+        
+        
+//        setTagsSelectedNewPost.contains(tagName) == true ? setTagsSelectedNewPost.insert(tagName) : setTagsSelectedNewPost.remove(tagName)
+        
+        
+        print(setTagsSelectedNewPost)
+        
+    }
+    
+    
 }
