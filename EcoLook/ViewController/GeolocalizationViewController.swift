941497c1ed596bac6b387cc9972894f35a1463df
Brian Sanchez <bsanchso@emeal.nttdata.com>
2022-07-02 16:59:31 -0500

    GeolocalizationViewController archivo y pantalla implementadas

---
+++ EcoLook/ViewController/GeolocalizationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GeolocalizationViewController.swift
+//  EcoLook
+//
+//  Created by Brian Antonio Sanchez Solorsano on 2/07/22.
+//
+
+import UIKit
+
+class GeolocalizationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
