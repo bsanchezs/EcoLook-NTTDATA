d142d2596b012b848fd62bcedb0b8861eec991ff
Brian Sanchez <bsanchso@emeal.nttdata.com>
2022-07-04 14:53:29 -0500

    variable del marker ubicado fuera del viewDidLoad para que se conozca en las extensiones y movimiento del mapa junto con el marcador implementado cuando se toca el boton de ir a ubicacion actual

--- EcoLook/ViewController/GeolocalizationViewController.swift
+++ EcoLook/ViewController/GeolocalizationViewController.swift
@@ -20,29 +20,23 @@ class GeolocalizationViewController: UIViewController {
     
     
     let geocoder = GMSGeocoder()
-
+    let marker = GMSMarker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let camera = GMSCameraPosition.camera(withLatitude: -8.1121075, longitude: -79.028297, zoom: 17.4)
         let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
+        
         mapView.isMyLocationEnabled = true
-        
-//        if let mylocation = mapView.myLocation {
-//            print("User's location: \(mylocation)")
-//        } else {
-//            print("User's location is unknown")
-//        }
-        
         mapView.settings.myLocationButton = true
-        
-//        print("mi ubicacion es ----------: \(String(describing: mapView.myLocation))")
+
         
         mapView.delegate = self
         self.view.addSubview(mapView)
 
         // Creates a marker in the center of the map.
-        let marker = GMSMarker()
+        
         marker.position = CLLocationCoordinate2D(latitude: -8.1121075, longitude: -79.028297)
         marker.map = mapView
         marker.isDraggable = true
@@ -67,9 +61,10 @@ extension GeolocalizationViewController: GMSMapViewDelegate{
                 
                 let lines = address.lines!
                 
-                print("address: \(address)")
-                print("direccion?: \(lines[0])")
-                print("tipo del lat: \(type(of: marker.position.latitude))")
+//                print("Marker moviendoseeeeeeee")
+//                print("address: \(address)")
+//                print("direccion?: \(lines[0])")
+//                print("tipo del lat: \(type(of: marker.position.latitude))")
                 
                 self.delegate?.markerDidChange(directionGeo: lines[0], latGeo: marker.position.latitude, lngGeo: marker.position.longitude)
             }
@@ -84,7 +79,33 @@ extension GeolocalizationViewController: GMSMapViewDelegate{
     func didTapMyLocationButton(for mapView: GMSMapView) -> Bool {
         print("Lo tocoooo!!")
 //        -8.103485946815995, -79.01666208299132 -- Coordenadas de mi casa
-        print("mi ubicacion es ----------: \(String(describing: mapView.myLocation))")
+        
+        guard let myLat = mapView.myLocation?.coordinate.latitude, let myLng = mapView.myLocation?.coordinate.longitude else{
+            return false
+        }
+        
+        let myCamera = GMSCameraPosition.camera(withLatitude: myLat, longitude: myLng, zoom: 17.4)
+        mapView.animate(to: myCamera)
+        
+        marker.position = CLLocationCoordinate2D(latitude: myLat, longitude: myLng)
+        marker.map = mapView
+        
+        geocoder.reverseGeocodeCoordinate(marker.position){ [self] response, error in
+            
+            if let address = response?.firstResult() {
+                
+                let lines = address.lines!
+                
+//                print("Marker moviendoseeeeeeee")
+//                print("direccion?: \(lines[0])")
+                
+                self.delegate?.markerDidChange(directionGeo: lines[0], latGeo: myLat, lngGeo: myLng)
+            }
+            
+        }
+    
+//        print("mi ubicacion es ----------: \(String(describing: mapView.myLocation))")
+//        print("mis coords son: \(myLat), \(myLng)")
 
         return true
     }
