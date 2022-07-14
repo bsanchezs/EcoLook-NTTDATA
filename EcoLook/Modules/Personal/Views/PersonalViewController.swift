0b1e39d6243f0e8fbeeeca17060f6ea2c2289147
Brian Sanchez <bsanchso@emeal.nttdata.com>
2022-07-14 15:07:49 -0500

    Correccion en el enlazado de elementos, el tableView no estaba enlazado. Extension del viewController con la funcion setConfigurationPersonalViewController para setear colores y estilos de los elementos. Extension del DataSource implementada sin el delegate y sin la imagen. Extension del delegate ButtonGoViewCellDelegate(cuerpo de la funcion no desarrollada aun)

--- EcoLook/Modules/Personal/Views/PersonalViewController.swift
+++ EcoLook/Modules/Personal/Views/PersonalViewController.swift
@@ -12,7 +12,8 @@ class PersonalViewController: UIViewController {
     
     @IBOutlet weak var labelTitleHeaderPersonal: UILabel!
     @IBOutlet weak var buttonGoNewPost: UIButton!
-    @IBOutlet weak var tableViewPersonalPostsCards: UIButton!
+    @IBOutlet weak var tableViewPersonalPostsCards: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +21,80 @@ class PersonalViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        
+//        presenter?.fetchFavoritesPostsByUser(idUser: 1)
+        
+    }
+    
     @IBAction func actionButtonGoNewPost(_ sender: Any) {
         
         
     }
+}
+
+extension PersonalViewController {
+    
+    func setConfigurationPersonalViewController() {
+        
+        self.view.backgroundColor = ThemeColors.verdeBackground
+        
+        labelTitleHeaderPersonal.textColor = ThemeColors.verdePrimary
+        
+        buttonGoNewPost.tintColor = ThemeColors.verdePrimary
+        
+        tableViewPersonalPostsCards.separatorStyle = .none
+        tableViewPersonalPostsCards.showsVerticalScrollIndicator = false
+        tableViewPersonalPostsCards.backgroundColor = ThemeColors.verdeBackground
+        tableViewPersonalPostsCards.dataSource = self
+        tableViewPersonalPostsCards.register(UINib(nibName: "PostsTableViewCell", bundle: nil), forCellReuseIdentifier: "customPostsCardsCell")
+        
+    }
+    
+    
+}
+
+extension PersonalViewController: UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customPostsCardsCell", for: indexPath) as? PostsTableViewCell
+        
+        let url = posts[indexPath.row].getUrlImage(imagen: posts[indexPath.row].imagen)
+        
+//        presenter?.fetchImageFavoritePostByUrl(url: url, cell: cell)
+        
+        cell?.labelTitleCard.text = posts[indexPath.row].titulo
+        cell?.labelShortDescCard.text = posts[indexPath.row].getShortDescription(descripcion: posts[indexPath.row].descripcion)
+        cell?.btnGoDetailCard.tag = posts[indexPath.row].idPublicacion
+        cell?.selectionStyle = .none
+        cell?.delegate = self
+        
+        return cell!
+        
+    }
+    
+    
+}
+
+extension PersonalViewController: ButtonGoViewCellDelegate{
+    
+    
+    func goToDetail(idPost: Int) {
+        
+//        presenter?.requestSendToDetail(idPost: idPost)
+
+    }
+    
+    
+    
 }
