96500a396402ffcbaa4dca14e13b62e21e5b1cc2
Brian Sanchez <bsanchso@emeal.nttdata.com>
2022-07-14 11:23:46 -0500

    Extensiones aplicadas al ViewController - DataSource del tableView(sin las imagenes ya que uso al presenter) y el ButtonGoViewCellDelegate(aun no implementado)

--- EcoLook/Modules/Favorites/Views/FavoritesViewController.swift
+++ EcoLook/Modules/Favorites/Views/FavoritesViewController.swift
@@ -36,9 +36,56 @@ extension FavoritesViewController {
         tableViewFavoritePostsCards.separatorStyle = .none
         tableViewFavoritePostsCards.showsVerticalScrollIndicator = false
         tableViewFavoritePostsCards.backgroundColor = ThemeColors.verdeBackground
-//        tableViewFavoritePostsCards.dataSource = self
+        tableViewFavoritePostsCards.dataSource = self
         tableViewFavoritePostsCards.register(UINib(nibName: "PostsTableViewCell", bundle: nil), forCellReuseIdentifier: "customPostsCardsCell")
         
     }
+    
+}
+
+extension FavoritesViewController: UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customPostsCardsCell", for: indexPath) as? PostsTableViewCell
+        
+        print("imagen: \(posts[indexPath.row].imagen)")
+        
+        let url = posts[indexPath.row].getUrlImage(imagen: posts[indexPath.row].imagen)
+        
+//        presenter?.fetchImagePostByUrl(url: url, cell: cell)
+        
+        cell?.labelTitleCard.text = posts[indexPath.row].titulo
+        cell?.labelShortDescCard.text = posts[indexPath.row].getShortDescription(descripcion: posts[indexPath.row].descripcion)
+        cell?.btnGoDetailCard.tag = posts[indexPath.row].idPublicacion
+        cell?.selectionStyle = .none
+        cell?.delegate = self
+        
+        return cell!
+        
+    }
+    
+    
+}
+
+extension FavoritesViewController: ButtonGoViewCellDelegate{
+    
+    
+    func goToDetail(idPost: Int) {
+        
+//        presenter?.requestSendToDetail(idPost: idPost)
+
+    }
+    
+    
     
 }
