77206f243c5561d5a6b4885c688fafaef76b9144
Brian Sanchez <bsanchso@emeal.nttdata.com>
2022-07-14 12:39:22 -0500

    Configuracion del modulo Favorites agregado e implementado, se realizaron las conexiones hacia las clases por medio de sus protocolos y se retorna la vista

--- EcoLook/Modules/Favorites/VIPERLayouts/FavoritesConfiguration.swift
+++ EcoLook/Modules/Favorites/VIPERLayouts/FavoritesConfiguration.swift
@@ -5,4 +5,36 @@
 //  Created by Brian Antonio Sanchez Solorsano on 14/07/22.
 //
 
-import Foundation
+import UIKit
+
+
+class FavoritesConfiguration {
+    
+    class func setFavoritesVIPER() -> UIViewController {
+
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let viewController = storyboard.instantiateViewController(withIdentifier: "FavoritesViewController") as? FavoritesViewController else {
+            fatalError("no se encontró viewcontroller")
+        }
+        
+        let favoritesPresenter = FavoritesPresenter()
+        let favoritesRouter = FavoritesRouter(currentViewController: viewController)
+        let favoritesInteractor = FavoritesInteractor()
+        
+        
+        viewController.presenter = favoritesPresenter
+        
+        favoritesPresenter.view = viewController
+        favoritesPresenter.interactor = favoritesInteractor
+        favoritesPresenter.router = favoritesRouter
+        
+        favoritesInteractor.presenter = favoritesPresenter
+        
+        favoritesRouter.presenter = favoritesPresenter
+        
+        return viewController
+
+
+    }
+    
+}
