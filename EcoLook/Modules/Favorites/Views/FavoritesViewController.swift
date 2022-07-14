04fd3daf0040c2264adc253c7556e0b03bdb2d9c
Brian Sanchez <bsanchso@emeal.nttdata.com>
2022-07-14 02:12:50 -0500

    Carpetas creadas en la seccion de modules referente al nuevo viewController(FavoritesViewController)

---
+++ EcoLook/Modules/Favorites/Views/FavoritesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesViewController.swift
+//  EcoLook
+//
+//  Created by Brian Antonio Sanchez Solorsano on 14/07/22.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
