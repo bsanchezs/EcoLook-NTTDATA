dad97959f43cb494b21005319ff2f5c3e57b191a
Brian Sanchez <bsanchso@emeal.nttdata.com>
2022-07-14 14:38:26 -0500

    Carpetas creadas referente al nuevo modulo (Personal) y archivo PersonalViewController agregado

---
+++ EcoLook/Modules/Personal/Views/PersonalViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PersonalViewController.swift
+//  EcoLook
+//
+//  Created by Brian Antonio Sanchez Solorsano on 14/07/22.
+//
+
+import UIKit
+
+class PersonalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
